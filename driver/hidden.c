e309119ea004b9dc1aa500d55e0bbc26ae1a14d1
mustaddon <mustaddon@gmail.com>
2026-04-26 09:02:55 +0300

    test

--- driver/hidden.c
+++ driver/hidden.c
@@ -11,7 +11,7 @@ UINT32_LE HiddenType(enum MESSAGE_TYPE type)
     CryptoRandom(&val, sizeof(val));
     
     rnd = ((UINT8)val)%245 + 7;
-    ((UINT8 *)&val)[0] = rnd + type - rnd%7;
-
+    //((UINT8 *)&val)[0] = rnd + type - rnd%7;
+    ((UINT8 *)&val)[0] = type;
     return val;
 }
--- driver/hidden.h
+++ driver/hidden.h
@@ -3,7 +3,8 @@
 #include "messages.h"
 #include "arithmetic.h"
 
-#define HEADER_TYPE_HIDDEN(Header) (((UINT8)(Header)->Type)%7)
+#define HEADER_TYPE_HIDDEN(Header) ((UINT8)((Header)->Type))
+//#define HEADER_TYPE_HIDDEN(Header) (((UINT8)(Header)->Type)%7)
 #define NBL_TYPE_HIDDEN(Nbl) HEADER_TYPE_HIDDEN((MESSAGE_HEADER *)MemGetValidatedNetBufferListData(Nbl))
 //#define NBL_TYPE_HIDDEN(Nbl) (((UINT8)((MESSAGE_HEADER *)MemGetValidatedNetBufferListData(Nbl))->Type)%7)
 
